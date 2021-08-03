459c917bc73fdd8b0c2ea4092eae237a02d73722
Sushanth <69962622+Sushanth47@users.noreply.github.com>
2021-08-03 14:33:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,62 @@
+require('dotenv').config
 const express = require('express')
-const app = express()
-// const cors = require('cors')
-// app.use(cors())
+const app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser')
+const cors = require('cors')
+app.use(cors());
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
+
 const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
-
-app.set('view engine', 'ejs')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+app.get('/', async(req, res) => {
+ return res.render('openingpage', { room: uuidV4() });
 })
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+app.post('/room', async (req, res) => {
+  var obj = {
+    table: {} 
+  }
+  obj.table= req.body.name
+  // obj.table.save()
+  console.log(obj.table, 'obj.table')
+  var json = JSON.stringify(obj)
+  fs.writeFile('./userdata.json', json, function (err) {
+    if (err) {
+      throw err;
+    }
+  });
+  fs.readFile('./userdata.json', 'utf8',function (err, jsonstring) {
+    if (err) throw err;
+    else {
+      try {
+        const customer = JSON.parse(jsonstring);
+        // console.log(customer, 'serverfiel');
+      return res.render('room', { roomId: req.params.room, jsonstring:customer});
+      }
+      catch (error){
+        console.log(error);
+      }
+      
+    }
+  });
+ 
 })
+
+
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
@@ -39,4 +74,6 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(process.env.PORT||3030)
+server.listen(process.env.PORT||3030, function () {
+  console.log(`Server is starting at port 3030`);
+})
