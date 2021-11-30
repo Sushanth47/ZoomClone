828491f9321367006e27895cb8c587b63b5f8eda
Sushanth47 <69962622+Sushanth47@users.noreply.github.com>
2021-11-30 14:33:05 +0530

    Done

--- public/script.js
+++ public/script.js
@@ -34,6 +34,7 @@ navigator.mediaDevices
     let text = $("input");
     // when press enter send message
     $("html").keydown(function (e) {
+      console.log("socket.emit");
       if (e.which == 13 && text.val().length !== 0) {
         socket.emit("message", text.val());
         text.val("");
